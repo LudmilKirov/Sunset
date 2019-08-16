4810e97df7f515bdb47cabd4dc32e218ed22f85f
LudmilKirov <ludmil9595@gmail.com>
2019-08-16 17:59:29 +0300

    I

---
+++ app/src/main/java/com/example/sunset/SingleFragmentActivity.java
@@ -0,0 +1,40 @@
+package com.example.sunset;
+
+import android.os.Bundle;
+
+import androidx.annotation.LayoutRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.sunset.R;
+
+//Set the activity view to be inflated from activity_fragment.xml.
+// Then look for the fragment in the Fragment manager in that
+// container,creating and adding it if does not exist
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+
+}
